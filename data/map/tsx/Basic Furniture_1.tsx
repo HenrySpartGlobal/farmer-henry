56162a16ba05ca1da224eed220f87fba254ec8a4
Henry <Henry_Koleoso@hotmail.co.uk>
2022-08-22 23:35:55 +0100

    import house

--- data/map/tsx/Basic Furniture_1.tsx
+++ data/map/tsx/Basic Furniture_1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.1" name="Basic Furniture_1" tilewidth="64" tileheight="64" tilecount="54" columns="9">
- <image source="../../../../../../Desktop/Sprout Lands - Sprites - premium pack/tilesets/Building parts/Basic Furniture_1.png" width="576" height="384"/>
+ <image source="../../../graphics/objects/Basic Furniture_1.png" width="576" height="384"/>
 </tileset>
--- data/map/tsx/Fences.tsx
+++ data/map/tsx/Fences.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.1" name="Fences (1)" tilewidth="64" tileheight="64" tilecount="32" columns="8">
- <image source="../../../../../../Desktop/Sprout Lands - Sprites - premium pack/tilesets/Building parts/Fences (1).png" width="512" height="256"/>
+ <image source="../../../graphics/objects/Fences (1).png" width="512" height="256"/>
 </tileset>
