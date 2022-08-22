c3c3a9f09fbb79d83caad5d33eb27c71e3bd2a56
Henry <Henry_Koleoso@hotmail.co.uk>
2022-08-22 17:56:43 +0100

    add more files

---
+++ data/map/tsx/Wooden House.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="Wooden House" tilewidth="64" tileheight="64" tilecount="1" columns="1">
+ <image source="../../../../../../Desktop/Sprout Lands - Sprites - premium pack/tilesets/Building parts/Wooden House.png" width="96" height="80"/>
+</tileset>
